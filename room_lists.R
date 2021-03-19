9f24d414d6c0da39a01a9fa16c098d4ebb9fa948
Adim Okwudishu <adimkris@gmail.com>
2021-03-19 14:17:59 -0400

    key-word lists for filtering data for room categories

---
+++ room_lists.R
@@ -0,0 +1,45 @@
+# Creating a list of potential categories from the Room Description and Area Description variables
+
+`Dinning/Cafeteria` <- c("Cafe|Cafetoria|Cafetorium|Dining|Kitchen|Lunch|Serving|Bistro|Food Service|Culinary")
+
+`Gymnasium/Gyms` <- c("Gym|Gymnasium|Gyms")
+
+`Auditorium` <- c("Auditorium|Large Group|Aud|1104|Capacity 450|Lower level|Lincoln Center|Rotunda")
+
+`Multipurpose Rooms` <- c("MPR|Multi-Purpose|Hall|Multipurpose|Multi-purpose|Choir|Chorus|Bluebonnet|Community|Gold Room
+                         |CJ Lynch|Eppley|Chap Court|2201|2407|1409|2412|D139|Chorus|E-1|Chap Room|WHITT|WHS|GLWD|GFLORA|LGI|Faith Bridge
+                          LS|RHSI|ESTC|Mc|Mall|Student Center|Annex|P-23")
+
+`Conference Rooms` <- c("Conference Hall|Conf|Conference Room|PH 702 Grant|PH 703 Harding")
+              
+`Class/Lecture Rooms` <- c("Exam Room|Classroom|Classrooms|Lecture Hall|Lecture|Training Room|F.T.I.
+                           |627|622|103|800|500|P-1|C126|Decatur Room 10|Classroom|Training Room|Lecture|Room 801|Room 7|Room 8
+                           |Room 224|Room|HH|SH|Room 131|1313|1309|1314|2403|2301|2303|2401|2205|29|100|101|102|P402|P403|P404
+                           |P405|P406|P407|H101|H104|Area C")
+                                                                            
+`General Meeting Rooms` <- c("Meeting|Board|Meeting Room|Annual Meeting|Monthly Meeting")                            
+                                                                             
+`Storage/Other Rooms` <- c("Booth|Locker|Office|Rm|Trauma|Nursing|Press Box|Coat Room|Storage|Office|Staff|Resource|Cabin 1|College & Career Center")                                                                       
+                                                                                                 
+`Media & Computer Labs` <- c("Computer|Lab|Science|Tech|Media|Training|Studio|Learning Center|Computer Lab|Smart Room
+                             |Laboratory|Work room|Game Room")
+
+`Library` <- c("Library|Lib")                                                                                                 
+                                  
+`Performing Arts` <- c("Theater|Theatre|Music|Stage|Band|Performing Arts Center|Arts|Dance|Dresssing|
+                       |Art|Black Box|Dressing|Scene|Orchestra|Rehearsal|Green Room|Amphitheater|Pavilion|Room 110|PAC
+                       |Band|Theater|Theatre|Dance|Drama|Amphitheater|1st Floor|Child Care|1011 Make Up")                                                                             
+                                                                                                                           
+`Open Areas` <- c("Concession|Commons|Common|Lobby|Open Area|Building|Lobby|Seating|Lounge|Foyer|Common Area|Quad|Outside Covered Area
+                  |Atrium|Outside|Lawn")                                                                                                                                
+
+`Portables/Restrooms` <- c("Portable|Restrooms|Restroom")                                                                                                            
+                                                                                                                                                                        
+`Grounds and Parking` <- c("Grounds|Garage|Parking|Lot")
+ 
+`Swimming Pools/Lanes` <- c("Swimming|Lane|Lanes|Pools|Pool|Swim|Natatorium|Natatoirum")                                                                                                                                                                                
+                                                                                                                                                                                    
+`Sports/Track & Field` <- c("Turf|Field|Fields|Track|Soccer|Baseball|BasketBall|VolleyBall|Wrestling|PE|Stadium|Tennis
+                           |Playground|Arena|Weight|Athletic|Softball|Court|Batting Cages|Cages|Bleachers|Field|Soccer|Athletic
+                           |Hard Court|Range Room")                                                                                                                                                                                                   
+                                                                                                                                                                                                        
